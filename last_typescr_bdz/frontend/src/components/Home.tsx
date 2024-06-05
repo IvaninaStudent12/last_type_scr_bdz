0d6fb060b44d67020ff7b968c3f8977859b2ad97
CakeCrake <maksimtomov10@gmail.com>
2024-06-05 14:53:47 +0300

    Home, NotFound, App

---
+++ last_typescr_bdz/frontend/src/components/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Home: React.FC = () => {
+    return (
+        <div>
+            <h2>Здравейте, това е уеб страница за запазване на билети за влак!</h2>
+            <h3>Всеки от нас имаше равно разпределени задачи като:</h3>
+            <h3>Frontend: Иванина и Максим</h3>
+            <h3>Backend: Мария</h3>
+            <h3>БД: Мария</h3>
+            <h3>UI/UX: Иванина</h3>
+            <h3>GitHub: Максим</h3>
+        </div>
+    );
+}
+
+export default Home;
